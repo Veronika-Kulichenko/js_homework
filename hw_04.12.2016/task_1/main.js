c9311a105cdd4fd7b33e4c94a4c53be810d636ef
Veronika-Kulichenko <nika.kulichenko@gmai.com>
2016-12-09 20:02:52 +0200

    added hw_04.12.2016

---
+++ hw_04.12.2016/task_1/main.js
@@ -0,0 +1,60 @@
+// Создать такой метод на прототипе конструктора Date, который будет принимать строку форматирования даты и возвращать строку с датой, отформатированную согласно условию.
+
+
+// Для форматирования могут быть приняты следующие сокращения:
+// YYYY - полный год
+// YY - последние две цифры года
+// MM - месяц, в цифровом представлении
+// MMMM - месяц, в словарном представлении на любом, удобном вам, языке
+// DD - день месяца
+// HH - часы, в 24 часовом представлении
+// mm - минуты
+// ss - секунды
+
+
+// Например,
+// someDate.getCustomFormat(“YY-MMMM-DD HH:mm:ss”);
+// // “16-september-13 21:00:00”
+
+
+	Date.prototype.getCustomFormat = function(format){
+
+	let currentDate = format;
+	
+	if(format.indexOf("YYYY") !== -1) {
+		let fullYear = this.getFullYear();
+		currentDate = currentDate.replace("YYYY", fullYear);
+	} 
+	if(format.indexOf("YY") !== -1) {
+		let shortYear = this.getFullYear().toString(10).substr(2,2);
+		currentDate = currentDate.replace("YY", shortYear);
+	} 
+	if(format.indexOf("MMMM") !== -1) {
+		var formatter = new Intl.DateTimeFormat("en-US", {
+			month: "long"});
+		let monthString = formatter.format(this);
+		currentDate = currentDate.replace("MMMM", monthString);
+	} 
+	if(format.indexOf("MM") !== -1) {
+		let monthNumber = this.getMonth() + 1;
+		currentDate = currentDate.replace("MM", monthNumber);
+	} 
+	if(format.indexOf("DD") !== -1) {
+		let day = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+		currentDate = currentDate.replace("DD", day);
+	} 
+	if(format.indexOf("HH") !== -1) {
+		let hours = this.getHours();
+		currentDate = currentDate.replace("HH", hours);
+	} 
+	if(format.indexOf("mm") !== -1) {
+		let minutes = this.getMinutes();
+		currentDate = currentDate.replace("mm", minutes);
+	} 
+	if(format.indexOf("ss") !== -1) {
+		let seconds = this.getSeconds();
+		currentDate = currentDate.replace("ss", seconds);
+	}
+}
+
+	
---
+++ hw_04.12.2016/task_2/main.js
@@ -0,0 +1,75 @@
+// Асинхронные запросы на сервер
+// Внимательно ознакомьтесь с API http://swapi.co.
+// Сценарий работы:
+// С помощью метода prompt() просим пользователя ввести номер персонажа (пока это от 1 до 87) по которому нужно получить информацию.
+
+
+// Если номер был введен неправильно, то сообщаем об этом пользователю.
+// Если номер введен верно, то формируем ответ, в котором:
+// Указано имя персонажа
+// Названия эпизодов, в которых участвовал персонаж
+// Название вида существ, к которому относится персонаж
+// Язык, присущий этому виду
+// Имена других представителей этого вида
+
+
+// Например,
+// Введено: 5
+
+
+// Ответ:
+// Name: Leia Organa
+// Films: Revenge of the Sith, Attack of the Clones, A New Hope
+// Species: Human
+// Language: "Galactic Basic"
+// Same origin: Luke Skywalker, ...
+
+
+let numberOfCharacter = prompt("Enter the character number");
+
+while(numberOfCharacter < 1 || numberOfCharacter > 87){
+	alert("Number should be from 1 to 87");
+	numberOfCharacter = prompt("Enter the character number");}
+
+	
+
+let peopleList = fetch(`http://swapi.co/api/people/${numberOfCharacter}`)
+.then(res => res.json())
+.then(function(res) {
+	let name = res.name,
+	films = res.films,
+	species = res.species;
+
+	let getFilms = films.map(film => fetch(film).then(res => res.json()));
+	return Promise.all(getFilms).then(res => ({name, films: res, species}));
+})	
+.then(function(res) {
+	let name = res.name,
+	films = res.films,
+	species = res.species;
+
+	let getSpecies = fetch(species[0]).then(res => res.json());
+	return Promise.all([getSpecies]).then(res => ({name, films, species: res}));
+})
+.then(function(res) {
+	let name = res.name,
+	films = res.films,
+	species = res.species[0].name;
+	language = res.species[0].language;
+
+	let sameOrigin = res.species[0].people.map(spicy => fetch(spicy).then(res => res.json()));
+	return Promise.all(sameOrigin).then(res => ({name, films, species, language, sameOrigin: res}));
+})
+.then(function(res) {
+	let name = res.name,
+	films = res.films,
+	species = res.species,
+	language = res.language,
+	sameOrigin = res.sameOrigin;
+
+	alert(`Name: ${name}\nFilms: ${films.map(film => film.title).join(", ")}\nSpecies: ${species}\nLanguage: ${language}\nSame origin: ${sameOrigin.map(spicy => spicy.name).join(", ")}`);
+});
+
+
+
+
